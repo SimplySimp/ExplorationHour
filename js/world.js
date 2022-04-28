8ad8dbb9e424f7c758399e1a03cff30619e04c46
Matthew Miller <m.miller@admin.attica.k12.in.us>
2022-04-28 10:23:15 -0400

    Added fade to battle start

--- js/world.js
+++ js/world.js
@@ -139,9 +139,8 @@ var WorldScene = new Phaser.Class({
         zone.y = Phaser.Math.RND.between(0, this.physics.world.bounds.height);
         
         // shake the world
-        this.physics.add.overlap(this.player, this.spawns, () => {
-			this.cameras.main.fade(700, 0, 0, 0, false);
-        }
+        this.cameras.main.shake(500); 
+		
         this.input.stopPropagation();
         // start battle 
         this.scene.switch('BattleScene');                
