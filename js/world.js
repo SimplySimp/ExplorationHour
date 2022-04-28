60a194c213e1173d61e988b2a06354f648e6ac50
Caden Turpin <c.turpin@admin.attica.k12.in.us>
2022-04-28 09:31:53 -0400

    Fixed Sprite walking anims

--- js/world.js
+++ js/world.js
@@ -69,7 +69,7 @@ var WorldScene = new Phaser.Class({
         //  animation with key 'left', we don't need left and right as we will use one and flip the sprite
         this.anims.create({
             key: 'left',
-            frames: this.anims.generateFrameNumbers('player', { frames: [1, 7, 1, 13]}),
+            frames: this.anims.generateFrameNumbers('player', { frames: [1, 4, 1, 7]}),
             frameRate: 10,
             repeat: -1
         });
@@ -77,19 +77,19 @@ var WorldScene = new Phaser.Class({
         // animation with key 'right'
         this.anims.create({
             key: 'right',
-            frames: this.anims.generateFrameNumbers('player', { frames: [1, 7, 1, 13] }),
+            frames: this.anims.generateFrameNumbers('player', { frames: [1, 4, 1, 7] }),
             frameRate: 10,
             repeat: -1
         });
         this.anims.create({
             key: 'up',
-            frames: this.anims.generateFrameNumbers('player', { frames: [2, 8, 2, 14]}),
+            frames: this.anims.generateFrameNumbers('player', { frames: [2, 5, 2, 8]}),
             frameRate: 10,
             repeat: -1
         });
         this.anims.create({
             key: 'down',
-            frames: this.anims.generateFrameNumbers('player', { frames: [ 0, 6, 0, 12 ] }),
+            frames: this.anims.generateFrameNumbers('player', { frames: [ 0, 3, 0, 6] }),
             frameRate: 10,
             repeat: -1
         });        
