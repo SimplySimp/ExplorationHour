76dfada119ce5cad9e0b81bc76c90fa1baa88f9f
Matthew Miller <m.miller@admin.attica.k12.in.us>
2022-04-29 08:51:26 -0400

    You can now run away

--- js/battle.js
+++ js/battle.js
@@ -92,7 +92,7 @@ var BattleScene = new Phaser.Class({
     receivePlayerSelection: function(action, target) {
         if(action == "attack") {            
             this.units[this.index].attack(this.enemies[target]);              
-        }else if (action == "run away") {
+        }else if (action == "Run Away") {
 		this.scene.switch('WorldScene')}
         // next turn in 3 seconds.
         this.time.addEvent({ delay: 3000, callback: this.nextTurn, callbackScope: this });        
