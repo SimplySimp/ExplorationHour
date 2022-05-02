b29a1bd5fa56145ebf4260276d519ff9f2a3d42b
Matthew Miller <m.miller@admin.attica.k12.in.us>
2022-05-02 09:47:58 -0400

    Changed battle switch to FadeIn

--- js/world.js
+++ js/world.js
@@ -139,7 +139,7 @@ var WorldScene = new Phaser.Class({
         zone.y = Phaser.Math.RND.between(0, this.physics.world.bounds.height);
         
         // shake the world
-        this.cameras.main.fade(300);
+        this.cameras.main.fadeIn(300);
         
         this.input.stopPropagation();
         // start battle 
