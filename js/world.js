dbb94a940b936a286a675a26a64f1d8c65457709
Matthew Miller <m.miller@admin.attica.k12.in.us>
2022-05-02 08:57:22 -0400

    Added fade to battle start

--- js/world.js
+++ js/world.js
@@ -138,11 +138,14 @@ var WorldScene = new Phaser.Class({
         zone.x = Phaser.Math.RND.between(0, this.physics.world.bounds.width);
         zone.y = Phaser.Math.RND.between(0, this.physics.world.bounds.height);
         
-        this.cameras.main.fadeIn(500, 0, 0, 0)
-	    this.cameras.main.once(Phaser.Cameras.Scene2D.Events.FADE_IN_COMPLETE, (cam, effect) => {
-		this.time.delayedCall(500, () => {
-			this.scene.start('BattleScene')
-		})
+         // Fade the world
+        this.cameras.main.fade(300);
+        
+        this.input.stopPropagation();
+        // start battle 
+        this.scene.switch('BattleScene');
+	    
+		
 	})   
        
 	},
