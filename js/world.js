b55508c9217294c749515623e4ca46ef020d0a6c
Matthew Miller <m.miller@admin.attica.k12.in.us>
2022-05-02 10:02:10 -0400

    Lengthened the FadeIn to 1100

--- js/world.js
+++ js/world.js
@@ -139,7 +139,7 @@ var WorldScene = new Phaser.Class({
         zone.y = Phaser.Math.RND.between(0, this.physics.world.bounds.height);
         
         // shake the world
-        this.cameras.main.fadeIn(700);
+        this.cameras.main.fadeIn(1100);
         
         this.input.stopPropagation();
         // start battle 
